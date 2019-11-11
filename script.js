4d87f23a87f2274dcb6d37f26128142b669d2519
ROUSHAN RAJ <roushan.raj656@gmail.com>
2019-11-11 11:08:10 +0530

    changed temp conv fn

--- script.js
+++ script.js
@@ -9,47 +9,10 @@ const cities = [
     {city:'Jaipur',state:',Rajasthan',degree :32,weather:'Sunny'}
 ];
 
-// function where updation from user selection is taking place
-// function changeCity(){
-//     let value=document.getElementById('in').value;
-//     cities.filter(arr=>{
-//         if(arr.city===value){
-//             document.getElementsByClassName("city")[0].innerHTML = arr.city;
-//             document.getElementsByClassName("state")[0].innerHTML = arr.state;
-//             document.getElementsByClassName("weather")[0].innerHTML = arr.weather;
-//             document.getElementsByClassName("temp")[0].innerHTML = arr.degree;
-//         }
-//     } );  
-
-// }
 const dayarr=["Sunday","Monday","Tuesday","WednesDay","Thursday","Friday","Saturday"];
 let currentDay= new Date();
 document.getElementById("day").innerHTML= dayarr[currentDay.getDay()];
 document.getElementById("time").innerHTML=currentDay.getHours()+":"+currentDay.getMinutes();
-// below is the code for degree conversion from celsius to farenheit and vice versa 
-let temperature = 23;
-let celsius = temperature;
-let farenheit;
-let celsiusCount = 0;
-
-function changeToFarenheit(){
-  document.getElementsByClassName("F")[0].style.color="#1890f0";
-  document.getElementsByClassName("C")[0].style.color="#000000";
-    if(celsiusCount < 1){
-        farenheit = Math.round(celsius*1.8+32);
-        temperature = farenheit;
-        celsiusCount++;
-    }
-  document.getElementsByClassName("temp")[0].innerHTML = farenheit;
-}
-
-function changeToCelsius(){
-    document.getElementsByClassName("C")[0].style.color="#1890f0";
-    document.getElementsByClassName("F")[0].style.color="#000000";
-    temperature = celsius;
-    celsiusCount--;
-    document.getElementsByClassName("temp")[0].innerHTML = celsius;
-}
 
 
 function weatherBalloon( ) {
@@ -70,6 +33,31 @@ function weatherBalloon( ) {
     // catch any errors
   });
 }
+// below is the code for degree conversion from celsius to farenheit and vice versa 
+let temperature = 0;
+let celsius = temperature;
+let farenheit;
+let celsiusCount = 0;
+
+function changeToFarenheit(){
+  document.getElementsByClassName("F")[0].style.color="#1890f0";
+  document.getElementsByClassName("C")[0].style.color="#000000";
+    if(celsiusCount < 1){
+        farenheit = Math.floor(celsius*1.8+32);
+        temperature = farenheit;
+        celsiusCount++;
+    }
+  document.getElementsByClassName("temp")[0].innerHTML = farenheit;
+}
+
+function changeToCelsius(){
+    document.getElementsByClassName("C")[0].style.color="#1890f0";
+    document.getElementsByClassName("F")[0].style.color="#000000";
+    temperature = celsius;
+    celsiusCount--;
+    document.getElementsByClassName("temp")[0].innerHTML = celsius;
+}
+
 
 
 // END 
